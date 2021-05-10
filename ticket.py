e895419eccb2ee9ec88c1c50efbdf5cb3b31eeea
Ali <59230675+acb94981@users.noreply.github.com>
2021-05-10 18:55:42 +0430

    algorithm ticket
    طراحی الگوریتم فرستادن تیکت برای آماتور های زبان پایتون  
    
    Designing a ticketing algorithm for Python language amateurs

---
+++ ticket.py
@@ -0,0 +1,27 @@
+#Tickets
+Username = input ("Username : ")
+Pass = input ("Password : ")
+
+print ("Login SuccessFully")
+print ("Login Account by username : ", Username)
+print ("<----for Admin---->")
+print ("Username : ", Username)
+print ("Password : ", Pass)
+
+First_name = input ("First name : ")
+Last_name = input ("Last Name : ")
+Topic = input ("Topic : ")
+Problem = input ("Problem : ")
+Email = input ("Email : @")
+
+print ("Ticket sent.")
+print ("Thanks say your Problem")
+
+print ("<----for Admin---->")
+
+print ("First name : ", First_name)
+print ("Last name : ", Last_name)
+print ("Topic : ", Topic)
+print ("Problem : ", Problem)
+print ("Email : @", Email)
+print ("Account username : ", Username)
